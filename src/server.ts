e40716e56a8aa3cd68cab29f47a95a03759976bc
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-09 20:44:25 +0700

    feat: adding swagger for api docs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
 import fastify from "fastify";
 import app from "./app";
 import { config } from "./config";
+import swagger from "@fastify/swagger";
+import swaggerUI from '@fastify/swagger-ui';
 
 const server = fastify({
   logger: true,
+});
+
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: 'JSHOP API',
+      description: 'API documentation for managing products & stock',
+      version: '1.0.0',
+    },
+    host:  `localhost:${config.PORT}`,
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+});
+
+server.register(swaggerUI, {
+  routePrefix: '/docs',
 });
 
 server.register(app);
