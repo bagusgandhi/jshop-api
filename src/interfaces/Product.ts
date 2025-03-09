b9819a51099e7ae1ed881484767710910d744e86
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-09 20:49:25 +0700

    feat: create interface Product and Product Dummy data

---
+++ src/interfaces/Product.ts
@@ -0,0 +1,20 @@
+export interface Product {
+  id: string,
+  title: string,
+  sku: string,
+  description?: string,
+  price: number,
+  image: string,
+  stock: number;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface ProductDummy {
+  id: string,
+  title: string,
+  sku: string,
+  description?: string,
+  price: number,
+  thumbnail: string,
+}
