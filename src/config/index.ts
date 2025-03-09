0e8a19d09f359dcc65bf20a54b036d33b397669d
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-09 20:47:11 +0700

    feat: add config  for dummyjson URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const config = {
   PORT: parseInt(process.env.PORT as string)|| 3000,
   DATABASE_URL: process.env.DATABASE_URL || '',
+  DUMMY_DATA_URL: process.env.DUMMY_DATA_URL || ''
 };
