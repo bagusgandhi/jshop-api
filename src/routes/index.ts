8e4d49ca3220e01946424065f29cec05e619ea35
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-09 20:53:28 +0700

    feat: add register route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { productRoutes } from "./productRoutes";
+import { adjusmentTransactionRoutes } from "./adjusmentTransactionRoutes";
 
 export const routes = async (fastify: FastifyInstance) => {
-  fastify.register(productRoutes, { prefix: "/api/products" });
+  fastify.register(productRoutes, { 
+    prefix: "/api/products" 
+  });
+  fastify.register(adjusmentTransactionRoutes, { 
+    prefix: "/api/adjustment-transactions" 
+  });
 };
